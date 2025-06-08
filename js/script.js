f8d6ac7eae96fb10b3d64cf30dac6677407ba1f8
Cleidy-Star <polaris.zm16@gmail.com>
2025-06-08 02:26:24 -0300

    Formularios criados, login e cadastro e contato

--- js/script.js
+++ js/script.js
@@ -5,4 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
     cpfInput.addEventListener('input', formatarCPF);
   }
 
- 
+   const formCadastro = document.getElementById('formCadastro');
+  if (formCadastro) {
+    formCadastro.addEventListener('submit', function(e) {
+      e.preventDefault();
+      concluirCadastro();
+    });
+  }
+
+  const formLogin = document.getElementById('formLogin');
+  if (formLogin) {
+    formLogin.addEventListener('submit', function(e) {
+      e.preventDefault();
+      loginUsuario();
+    });
+  }
+
+  const formContato = document.getElementById('formContato');
+  if (formContato) {
+    formContato.addEventListener('submit', function(e) {
+      e.preventDefault();
+      enviarMensagemContato();
+    });
+  }
+});
